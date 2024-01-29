53c3f0242462137a41c96765e96be2fd26d6b5f3
icode <icode1211@naver.com>
2024-01-29 17:17:10 +0900

    chore: DenyLogicOperatorExample

--- src/chapter03/DenyLogicOperatorExample.java
+++ src/chapter03/DenyLogicOperatorExample.java
@@ -10,9 +10,5 @@ public class DenyLogicOperatorExample {
 
         play = !play;
         System.out.println(play);
-
-        char c1 = 'A' + 1;   // 'B' (유니코드 66은 문자 B)
-        char c2 = 'A';
-        char c3 = (char) (c2 + 1);   // 에러
     }
 }
